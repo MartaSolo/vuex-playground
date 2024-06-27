229982dfc58ed48d4558aea4a2e91996c09ebf69
Marta Solomacha <marta.solomacha@monterail.com>
2024-06-27 20:56:35 +0200

    Remove setup data and add airports

---
+++ src/types/airport.ts
@@ -0,0 +1,6 @@
+export interface Airport {
+  name: string
+  abbreviation: string
+  city: string
+  state: string
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,39 @@
+<template>
+  <div class="wrapper">
+    <div v-for="airport in airports" :key="airport.abbreviation">
+      <airport-card :airport="airport" />
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+import allAirports from '@/data/airports.js'
+import AirportCard from '@/components/AirportCard.vue'
+
+const airports = ref(allAirports)
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+p,
+h3 {
+  grid-column: span 3;
+}
+</style>
